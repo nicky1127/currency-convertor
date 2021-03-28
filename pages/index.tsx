ed910a99436dc8e49b2129e1019c59ed1afbae68
Nicky Lai <nicky.lai@lloydsbanking.com>
2021-03-28 18:15:57 +0100

    Create BgWrapper for background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const BgContainer = styled.div`
+  width: 100%;
+  height: 60vh;
+  background-color: ${({ colour }) => colour};
+`;
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>TWD Convertor</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Wrapper>
+        <BgContainer colour="#0a2239" />
+        <BgContainer colour="#F1DEDE" />
+      </Wrapper>
     </div>
   );
 }
